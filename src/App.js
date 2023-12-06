efc702427bef27d74c58ee1d9e4e492f07d28821
ishaikh37 <ishaikh@tenupsoft.com>
2023-12-06 19:59:40 +0530

    Added modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 // src/App.js
 import React from "react";
 import { Button } from "reactstrap";
+import CustomUI from "./MyModal";
 // import CustomUI from './MyModal';
 
 function App() {
   return (
-    <Button style={{ margin: "10px" }} color="primary">
-      Apply to finance
-    </Button>
+    // <Button style={{ margin: "10px" }} color="primary">
+    //   Apply to finance
+    // </Button>
+    <CustomUI />
   );
 }
 
--- src/MyModal.js
+++ src/MyModal.js
@@ -1,57 +1,31 @@
-// src/CustomUI.js
-import React from 'react';
-import IconImage from "./asset/Image.png";
+import React, { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
 const CustomUI = () => {
-  const styles = {
-    Card: {
-      top: '114px',
-      left: '526px',
-      width: '388px',
-      height: '83px',
-      backgroundColor: '#ffffff',
-      borderRadius: '16px',
-      boxShadow: '0px 1px 12px rgba(3,3,3,0.08)',
-    },
-    ImageContainer: {
-      top: '130px',
-      left: '543px',
-      width: '47px',
-      height: '49px',
-      borderRadius: '100px',
-      backgroundImage: 'url(./image.png)',
-      backgroundPosition: 'center center',
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-    },
-    Text: {
-      color: '#030303',
-      fontSize: '14px',
-      fontFamily: 'Poppins',
-      fontWeight: '500',
-      lineHeight: '17px',
-    },
-    Text2: {
-      color: '#858585',
-      fontSize: '12px',
-      fontFamily: 'Poppins',
-      fontWeight: 300,
-      lineHeight: '16px',
-    },
-  };
+  const [modal, setModal] = useState(true);
+
+  const toggle = () => setModal(!modal);
   return (
-    <div style={styles.Card}>
-      <div style={{
-      ...styles.ImageContainer,
-      backgroundImage: `url(${IconImage})`,
-    }} />
-    <div style={styles.Text}>
-    Get it on finance from £30 per month
-    </div>
-    <div style={styles.Text2}>
-    Instant decisions ~ 96% Approval Rate
-    </div>
-    </div>
+    <Modal isOpen={true} toggle={toggle}>
+      <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+      <ModalBody>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </ModalBody>
+      <ModalFooter>
+        <Button color="primary" onClick={toggle}>
+          Do Something
+        </Button>{" "}
+        <Button color="secondary" onClick={toggle}>
+          Cancel
+        </Button>
+      </ModalFooter>
+    </Modal>
   );
 };
 
